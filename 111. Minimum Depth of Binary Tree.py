876dd237a0dd59275276687164580077b11ac456
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-10-22 21:42:20 +0530

    Create 111. Minimum Depth of Binary Tree.py

---
+++ 111. Minimum Depth of Binary Tree.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        # DFS
+        if not root:
+            return 0
+        
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        
+        if not root.right:
+            return self.minDepth(root.left) + 1
+        
+        else:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
